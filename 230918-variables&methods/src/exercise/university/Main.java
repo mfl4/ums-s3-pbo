c24806adb8e689345d238e2fe5491449857d19ba
mfl4 <mhdfarhanl6267@gmail.com>
2023-11-02 11:14:57 +0700

    thursday-2nd-november-2023

---
+++ 230918-variables&methods/src/exercise/university/Main.java
@@ -0,0 +1,8 @@
+package exercise.university;
+
+public class Main {
+    public static void main(String[] args) {
+        Student std1 = new Student(); 
+        System.out.println(getStudentName("John"));
+    }
+};
