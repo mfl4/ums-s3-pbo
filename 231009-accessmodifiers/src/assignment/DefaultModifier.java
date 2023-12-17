c32fed019ae892bbffa91c345af4d18490a5d51c
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-17 09:28:59 +0700

    sunday-17th-december-2023

--- 231009-accessmodifiers/src/assignment/DefaultModifier.java
+++ 231009-accessmodifiers/src/assignment/DefaultModifier.java
@@ -1,11 +1,11 @@
 package assignment;
 
 public class DefaultModifier {
-    private int x = 10;
-    private int y = 20;
-    private int z = x + y;
+    int x = 10;
+    int y = 20;
+    int z = x + y;
 
-    private void multiply() {
+    void multiply() {
         System.out.println("Default Multiplication of " + x + ", " + y + " and " + z + " is " + (x * y * z));
     }
 }
