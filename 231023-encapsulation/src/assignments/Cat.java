846d0932c040f56e5056f86781fed2a6c986268a
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-29 22:21:24 +0700

    [PUSH] feat: friday-29th-december-2023

---
+++ 231023-encapsulation/src/assignments/Cat.java
@@ -0,0 +1,13 @@
+package assignments;
+
+public class Cat {
+    private String type;
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
