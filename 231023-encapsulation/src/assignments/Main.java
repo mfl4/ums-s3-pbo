619c7e9c2a0c9dd09fbe7cf3c88e853e4387ba53
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-29 22:22:09 +0700

    [PUSH] feat: friday-29th-december-2023

---
+++ 231023-encapsulation/src/assignments/Main.java
@@ -0,0 +1,10 @@
+package assignments;
+
+public class Main extends Cat {
+    public static void main(String[] args) {
+        Cat lily = new Cat();
+
+        lily.setType("Munchkin");
+        System.out.println(lily.getType());
+    }
+}
