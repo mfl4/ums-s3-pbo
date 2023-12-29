1f268adb7a5c1cbed352d9f7dd651f9574cc36ed
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-29 21:44:24 +0700

    [PUSH] feat: friday-29th-december-2023

---
+++ 231009-accessmodifiers/src/assignment/MainInner.java
@@ -0,0 +1,17 @@
+package assignment;
+
+public class MainInner {
+    public static void main(String[] args) {
+        AllModifier object = new AllModifier();
+
+        object.w = 10;
+        object.x = 20;
+        object.y = 30;
+        object.z = 40;
+
+        object.multiply();
+        object.divide();
+        object.add();
+        object.subtract();
+    }
+}
